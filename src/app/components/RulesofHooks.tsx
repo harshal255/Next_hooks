a195dd46c52936829fd174f071906147a2a567b0
harshal255 <200280107119@ldceahm.gujgov.edu.in>
2023-09-19 22:36:35 +0530

    Rules of hooks 📚

---
+++ src/app/components/RulesofHooks.tsx
@@ -0,0 +1,17 @@
+//1️⃣ : Always write it inside the component or functions
+//2️⃣ : Component name must be PascalCase (first letter should be uppercase);
+//3️⃣ : we can directly import or we can directly write it using React.hookName;
+//4️⃣ : Don't call Hooks inside loops, conditions, or nested functions,but we can use hooks variable like setName inside of loops, conditions or nested functions
+
+import React from 'react'
+
+const RulesofHooks = () => {
+    return (
+        <div>
+            Hooks of ReactJS
+        </div>
+    )
+}
+
+export default RulesofHooks
+  
