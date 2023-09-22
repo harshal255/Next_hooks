297eb71f1b3e37de5317d0d4243a4f3d49a07566
harshal255 <200280107119@ldceahm.gujgov.edu.in>
2023-09-22 21:58:39 +0530

    useeffect feat  💡

---
+++ src/app/useeffect-hooks/page.tsx
@@ -0,0 +1,25 @@
+'use client'
+import React, { useState, useEffect } from 'react'
+
+
+const Page = () => {
+    const [count, setCount] = useState(0);
+    //useEffect used for sideeffect of outside of components (rerender our component)
+    //reactjs given first priority console.log in component & then useEffect function
+    useEffect(() => {
+        console.log("calling useeffect");
+        document.title = `Chats (${count})`;
+
+    })
+    return (
+        <div className='flex flex-col w-full h-[100vh] items-center justify-center gap-10'>
+            <div className='text-5xl font-semibold text-green-400'>
+                I am UseEffect Hook
+                <span>{count}</span>
+            </div>
+            <button className='text-yellow-400 border border-white p-3' onClick={() => setCount(count + 1)}>Change count</button>
+        </div>
+    )
+}
+
+export default Page
